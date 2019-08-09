8df44a3ca1e9057bf562799ff60deca0e158e2d7
David <davidguilarte7@gmail.com>
2019-08-09 01:15:04 -0430

    Matiannys

---
+++ app/Http/Controllers/API/AttachmentController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Attachment;
+use App\Http\Resources\Attachment as AttachmentResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class AttachmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $attachments = Attachment::all();
+        return AttachmentResource::collection( $attachments );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $attachment = Attachment::create([
+          'consultation_id' => $request->consultation_id,
+          'url'             => $request->url,
+          'date'            => Carbon::now()
+        ]);
+        
+        return new AttachmentResource( $attachment );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Attachment  $attachment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attachment $attachment)
+    {
+        return new AttachmentResource( $attachment );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Attachment  $attachment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attachment $attachment)
+    {
+      $attachment->update([
+        'url'   => $request->url,
+        'date'  => Carbon::now()
+      ]);
+      
+      return new AttachmentResource( $attachment );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Attachment  $attachment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attachment $attachment)
+    {
+        $attachment->delete();
+        return response()->json(['message'=>'Attachment deleted successfuly']);
+    }
+}
---
+++ app/Http/Requests/StoreConsultationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreConsultationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'doctor_id'  => 'required|integer',
+            'patient_id' => 'required|integer',
+            'product_id' => 'required|integer',
+            'revisor_id' => 'nullable|integer',
+            'condition'  => 'required|integer'
+        ];
+    }
+}
