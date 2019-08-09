e7d7822eaae6f59fd84dddd8aa09ef413c19fd22
David <davidguilarte7@gmail.com>
2019-08-09 00:50:29 -0430

    API Patient Controller with resource

---
+++ app/Http/Controllers/API/Admin/DoctorController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Doctor;
+use App\User;
+use App\Http\Resources\Doctor as DoctorResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class DoctorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $doctors = Doctor::all();
+        return DoctorResource::collection( $doctors );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create([
+          'name'  => $request->name,
+          'email' => $request->email,
+          'password'  => bcrypt($request->password)
+        ]);
+        
+        if( $user ){
+          $doctor = Doctor::create([
+            'speciality' => $request->speciality,
+            'user_id'    => $user->id
+          ]);
+        }
+        
+        return new DoctorResource( $doctor );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Doctor  $doctor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Doctor $doctor)
+    {
+        return new DoctorResource( $doctor );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Doctor  $doctor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Doctor $doctor)
+    {
+      $doctor->update([
+        'speciality'  => $request->speciality
+      ]);
+      
+      return new DoctorResource( $doctor );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Doctor  $doctor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Doctor $doctor)
+    {
+        $doctor->delete();
+        return response()->json(['message'=>'Doctor deleted successfuly']);
+    }
+}
---
+++ app/Http/Controllers/API/PatientController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Patient;
+use App\User;
+use App\Http\Resources\Patient as PatientResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class PatientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $patients = Patient::all();
+        return PatientResource::collection( $patients );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = User::create([
+          'name'      => $request->name,
+          'email'     => $request->email,
+          'password'  => bcrypt($request->password)
+        ]);
+        
+        if( $user ){
+          $patient = Patient::create([
+            'birthdate' => $request->birthdate,
+            'weight'    => $request->weight,
+            'height'    => $request->height,
+            'user_id'   => $user->id
+          ]);
+        }
+        
+        return new PatientResource( $patient );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Patient $patient)
+    {
+        return new PatientResource( $patient );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Patient $patient)
+    {
+      $patient->update([
+        'name'        => $request->name,
+        'description' => $request->description,
+      ]);
+      
+      return new PatientResource( $patient );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Patient  $patient
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Patient $patient)
+    {
+        $patient->delete();
+        return response()->json(['message'=>'Patient deleted successfuly']);
+    }
+}
